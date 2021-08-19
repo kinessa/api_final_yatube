535af03eee82afb75bf61a92372f594e37854736
Inessa <0309kinessa@gmail.com>
2021-08-19 13:35:52 +0300

    первая попытка

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,4 +1,5 @@
-from rest_framework.permissions import (IsAuthenticatedOrReadOnly, SAFE_METHODS)
+from rest_framework.permissions import \
+    (IsAuthenticatedOrReadOnly, SAFE_METHODS)
 
 
 class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
